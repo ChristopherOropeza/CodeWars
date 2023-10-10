d4954b47e63f4b705f00603d8c848c59aee4dd85
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-10-09 19:57:19 -0700

    Create xO.js
    CodeWars problem and solution.

---
+++ xO.js
@@ -0,0 +1,16 @@
+// Task:
+/* Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples of input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false */
+
+// Solution:
+
+function xO(str) {
+    return (str.toLowerCase().split('x').length == str.toLowerCase().split('o').length);
+}
