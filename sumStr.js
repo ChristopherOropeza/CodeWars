6e989edb89e39f5ba45cbcb7ef6354bcb504fbfe
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-10-14 00:27:50 -0700

    Create sumStr.js
    CodeWars problem and solution

---
+++ sumStr.js
@@ -0,0 +1,16 @@
+//Task:
+
+/* Create a function that takes 2 integers in the form of a string as an input, and outputs the sum (also as a string):
+
+Example: (Input1, Input2 --> Output)
+"4",  "5" --> "9"
+"34", "5" --> "39"
+"", "" --> "0"
+"2", "" --> "2"
+"-5", "3" --> "-2" */
+
+// Solution:
+
+function sumStr(a,b) {
+  return (Number(a) + Number(b)).toString();
+}
