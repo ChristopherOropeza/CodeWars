5792afb070a78d0606589e1258176c009272005a
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-10-20 20:17:52 -0700

    Create shortLongShort.js
    Codewars problem and solution

---
+++ shortLongShort.js
@@ -0,0 +1,18 @@
+// Task:
+
+/* Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+For example: (Input1, Input2) --> output
+
+("1", "22") --> "1221"
+("22", "1") --> "1221" */
+
+// Solution:
+
+function shortLongShort(a, b){
+  if (a.length > b.length) {
+    return b + a + b;
+  } else if (b.length > a.length) {
+    return a + b + a;
+  }
+}
