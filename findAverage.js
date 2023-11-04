09a047b7ac57fd7d0bc8bd3b8b32fb3c2b08d7d9
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-11-04 02:14:13 -0700

    Create findAverage.js
    Codewars problem and solution.

---
+++ findAverage.js
@@ -0,0 +1,11 @@
+// Task:
+
+/* Write a function that calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0. */
+
+// Solution:
+
+function findAverage(array) {
+  return array.length === 0 ? 0 : array.reduce((acc, c) => acc + c, 0) / array.length;
+}
