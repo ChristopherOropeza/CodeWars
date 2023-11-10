ff955e884dd79009a8024433d4b15982be027ae7
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-11-10 00:42:35 -0800

    Create invert.js
    Codewars problem and solution.

---
+++ invert.js
@@ -0,0 +1,21 @@
+// Problem:
+
+/* Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+invert([]) == [] */
+
+// Solution:
+
+function invert(array) {
+   return array.map(number => number/-1);
+}
+
+// Another solution:
+
+/*
+function invert(array) {
+   return array.map(number => -number);
+}
+*/
