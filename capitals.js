25bbcbbb00c1097e6e9800b38646b99ad57ff342
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-11-30 13:04:09 -0800

    Create capitals.js
    Codewars problem and solution.

---
+++ capitals.js
@@ -0,0 +1,13 @@
+// Problem:
+
+/* Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+
+Example (Input --> Output):
+
+"CodEWaRs" --> [0,3,4,6] */
+
+// Solution:
+
+function capitals(word) {
+  return word.split('').reduce((indices,char,i) => char === char.toUpperCase() ? [...indices,i] : indices,[]);
+}
