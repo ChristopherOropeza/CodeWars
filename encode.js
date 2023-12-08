f7131c907d5e6c215b8672caac7654f7017b6988
ChristopherOropeza <corpe1994@gmail.com>
2023-12-08 13:42:52 -0800

    Create encode.js

---
+++ encode.js
@@ -0,0 +1,43 @@
+// Task:
+
+/* Introduction
+The GADERYPOLUKI is a simple substitution cypher used in scouting to encrypt messages. The encryption is based on short, easy to remember key. The key is written as paired letters, which are in the cipher simple replacement.
+
+The most frequently used key is "GA-DE-RY-PO-LU-KI".
+
+ G => A
+ g => a
+ a => g
+ A => G
+ D => E
+  etc.
+The letters, which are not on the list of substitutes, stays in the encrypted text without changes.
+
+Task
+Your task is to help scouts to encrypt their messages. Write the Encode function.
+
+Input/Output
+The input string consists of lowercase and uppercase characters and whitespace. The substitution has to be case-sensitive.
+
+Example
+ encode("ABCD")          // => GBCE 
+ encode("Ala has a cat") // => Gug hgs g cgt 
+ encode("gaderypoluki"); // => agedyropulik
+ decode("Gug hgs g cgt") // => Ala has a cat 
+ decode("agedyropulik")  // => gaderypoluki
+ decode("GBCE")          // => ABCD */
+
+// Solution:
+
+const dict = {
+    G: "A", A: "G", D: "E", E: "D", 
+    R: "Y", Y: "R", P: "O", O: "P", 
+    L: "U", U: "L", K: "I", I: "K", 
+    g: "a", a: "g", d: "e", e: "d", 
+    r: "y", y: "r", p: "o", o: "p", 
+    l: "u", u: "l", k: "i", i: "k", 
+}
+  
+function encode(message){
+    return message.replace(/./g, c => dict[c] || c);
+}
