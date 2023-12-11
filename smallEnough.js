bd45215370f2b99aa58e357c5d67172edc63b499
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2023-12-11 11:07:02 -0800

    Create smallEnough.js
    Codewars problem and solution.

---
+++ smallEnough.js
@@ -0,0 +1,11 @@
+// Problem:
+
+/* You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers. */
+
+// Solution:
+
+function smallEnough(arr, limit){
+  return arr.every(val => val <= limit);
+}
