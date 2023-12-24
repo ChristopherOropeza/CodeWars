efd464df790540f5b48a3b2d9b1f3e6283b9c631
ChristopherOropeza <corpe1994@gmail.com>
2023-12-23 20:11:15 -0800

    Create isIntArray.js

---
+++ isIntArray.js
@@ -0,0 +1,17 @@
+// Problem:
+
+/* Write a function with the signature shown below:
+
+function isIntArray(arr) {
+  return true
+}
+
+returns true  / True if every element in an array is an integer or a float with no decimals.
+returns true  / True if array is empty.
+returns false / False for every other input. */
+
+// Solution:
+
+function isIntArray(arr) {
+  return Array.isArray(arr) && arr.every(el => Math.floor(el) === el);
+}
