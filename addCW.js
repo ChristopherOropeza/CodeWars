2ce3fdbee23a7dbe275e6233a9777ef8bf8a8367
ChristopherOropeza <corpe1994@gmail.com>
2024-01-17 16:16:16 -0800

    Create addCW.js

---
+++ addCW.js
@@ -0,0 +1,14 @@
+// Problem:
+
+/* Add the value "codewars" to the websites array.
+After your code executes the websites array should == ["codewars"] */
+
+// Solution:
+
+const addCW = (el) => {
+    let websites = [];
+    if (el === 'codewars') {
+        websites.push(el);
+    }
+    return websites;
+}
