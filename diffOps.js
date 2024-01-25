46007de7e5f3b90bb222e8d45cbf6301dff32bac
ChristopherOropeza <corpe1994@gmail.com>
2024-01-24 17:07:53 -0800

    Added ability to calculate modulus

--- diffOps.js
+++ diffOps.js
@@ -5,11 +5,14 @@
 The function should take three arguments - operation(string/char), value1(number), value2(number).
 The function should return result of numbers after applying the chosen operation.
 
+Edit: Also added ability to do calculate modulus.
+
 Examples(Operator, value1, value2) --> output
 ('+', 4, 7) --> 11
 ('-', 15, 18) --> -3
 ('*', 5, 5) --> 25
-('/', 49, 7) --> 7 */
+('/', 49, 7) --> 7 
+('%', 6, 6) --> 0 */
 
 // Solution:
 
@@ -27,7 +30,13 @@ function basicOp(operation, value1, value2){
             } else {
                 return "Cannot divide by zero";
             }
+        case '%':
+            if (value2 !== 0) {
+                return value1 % value2;
+            } else {
+                return "Cannot calculate modulus with a divisor of zero";
+                }
         default:
-            return `0: Error, ${operation} is not an operation.`;
+            return `Error, ${operation} is not an operation. You can only input the following 5 symbols: '+, -, *, /, %'.`;
     }
 }
