78e55c56cd1d0c5368ca8ecd4c8d0dcf856c0c6d
ChristopherOropeza <corpe1994@gmail.com>
2024-03-10 15:21:39 -0700

    Formatting

--- checkAlive.js
+++ checkAlive.js
@@ -2,7 +2,7 @@
 
 /* While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive. Unfortunately, Greg made some errors while creating the function.
 
-The function checkAlive should return true if the player's health is greater than 0 or false if it is 0 or below.
+The function checkIfAlive should return true if the player's health is greater than 0 or false if it is 0 or below.
 
 The function receives one parameter called "health", which will always be a whole number between -10 and 10. */
 
