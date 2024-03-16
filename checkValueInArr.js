40809b69ada5777a0a155023983cfd0c510cb7c6
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2024-03-16 13:33:59 -0700

    Update and rename check.js to checkValueInArr.js

--- checkValueInArr.js
+++ checkValueInArr.js
@@ -1,11 +1,11 @@
 // Problem:
 
-/* You will be given an array a and a value x. All you need to do is check whether the provided array contains the value, without using a loop.
+/* You will be given an array `a` and a value `x`. All you need to do is check whether the provided array contains the value, without using a loop.
 
 The array `a` can contain numbers or strings. The value `x` can be either. Return true if the array contains the value, otherwise return false if it does not. With strings, you will need to account for case. */
 
 // Solution:
 
-function check(a,x) {
+function checkValueInArr(a,x) {
     return a.includes(x) ? true : false;
 }
