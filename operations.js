24959d93d6c4c63a0699882003e12d2af2b3382b
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2024-05-04 23:17:43 -0700

    Update operations.js

--- operations.js
+++ operations.js
@@ -1,6 +1,6 @@
 // Problem:
 
-Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+/* Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
 
 Please use the following function names:
 
@@ -16,7 +16,7 @@ exponential = exponent
 
 subtraction = subt
 
-Note: All math operations will be: a (operation) b
+Note: All math operations will be: a (operation) b */
 
 // Solution:
 
