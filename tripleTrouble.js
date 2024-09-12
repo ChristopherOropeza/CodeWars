1fb482a5833f722d060ab0245c0a315f4b9e94b5
Chris Oropeza <117047751+ChristopherOropeza@users.noreply.github.com>
2024-09-12 12:46:54 -0700

    Update tripleTrouble.js

--- tripleTrouble.js
+++ tripleTrouble.js
@@ -1,6 +1,6 @@
 // Problem:
 
-/* Create a function that will return a string that combines all of the letters of the three input strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see the example below!
+/* Create a function that will return a string that combines all of the letters of the three input strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see the example below.
 
 E.g. Input: "aa", "bb", "cc" => Output: "abcabc"
 
